96e4d8e9b2a3f2acec6f502037f3d9dfed6ac22e
Steven Canning <spcanning@gmail.com>
2013-09-17 00:28:31 -0700

    Started to define Polynomial type

---
+++ polynomials.hs
@@ -0,0 +1,8 @@
+data Polynomial = Polynomial [Int]
+	deriving (Eq, Show)
+
+instance Num (Polynomial) where
+	Polynomial [] + Polynomial a = Polynomial a
+	Polynomial a + Polynomial [] = Polynomial a
+	Polynomial (a:as) + Polynomial (b:bs) =
+		let Polynomial h = Polynomial as + Polynomial bs in Polynomial ((a+b):h)
