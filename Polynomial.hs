1a14f0e06e42b24d414c6f06e3f9fd37df3b22ff
Steven Canning <spcanning@gmail.com>
2013-09-19 00:47:00 -0700

    An improved show for Polynomials

--- Polynomial.hs
+++ Polynomial.hs
@@ -1,7 +1,7 @@
 module Polynomial where
 
 data Polynomial = Polynomial [Int]
-	deriving (Eq, Show)
+	deriving (Eq)
 
 instance Num (Polynomial) where
 	Polynomial [] + Polynomial [] = Polynomial []
@@ -9,3 +9,12 @@ instance Num (Polynomial) where
 	Polynomial a + Polynomial [] = Polynomial a
 	Polynomial (a:as) + Polynomial (b:bs) =
 		let Polynomial h = Polynomial as + Polynomial bs in Polynomial ((a+b):h)
+
+instance Show Polynomial where
+	show (Polynomial []) = "0"
+	show (Polynomial (p:ps)) = 
+		let
+			makePolyStr(_, []) = ""
+			makePolyStr(n, 0:ls) = makePolyStr(n+1, ls)
+			makePolyStr(n, l:ls) = " + " ++ (show l) ++ "x**" ++ (show n) ++ makePolyStr(n+1, ls)
+		in (show p) ++ makePolyStr(1, ps)
